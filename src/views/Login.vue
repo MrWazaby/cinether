39b13dfead11f5ea2f93285175f5f5a4fe51e345
Alexandre Martin <alexandre@martin.pm>
2022-09-05 09:21:14 +0200

    refactor(login): refactor the login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,20 +5,30 @@
         <ion-title>Cinether</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content>
-      <ion-input placeholder="Login"></ion-input>
+    <ion-content class="ion-padding">
+      <ion-input placeholder="Email"></ion-input>
       <ion-input placeholder="Password" type="password"></ion-input>
-      <ion-button expand="block">Login</ion-button>
+      <ion-button expand="block"><ion-icon :icon="logInOutline" class="ion-margin-end"></ion-icon>Login</ion-button>
+      <ion-button expand="block" color="light" id="open-register-modal"><ion-icon :icon="personAddOutline" class="ion-margin-end"></ion-icon> Register</ion-button>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/vue';
+import { logInOutline, personAddOutline } from 'ionicons/icons';
 
 export default  defineComponent({
-  name: 'Tab1Page',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  name: 'LoginPage',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonIcon },
+  setup() {
+    return {
+      logInOutline,
+      personAddOutline
+    }
+  }
 });
+
 </script>
